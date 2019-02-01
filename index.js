89d3d38eec90b7229e04a919b3cacfe4534106f0
Maik Hummel <maik@neohelden.com>
2019-02-01 12:40:00 +0100

    Add mustache module

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ module.exports = {
   log: require('./lib/log'),
   task: require('./lib/task'),
   i18n: require('./lib/i18n'),
-  got: require('./lib/got')
+  got: require('./lib/got'),
+  mustache: require('mustache')
 }
